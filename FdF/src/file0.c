07fea911e8b95f61f0685501d893459bb08328f1
qbnt <quentin.banet@hotmail.fr>
2023-06-20 14:23:31 +0200

    f

--- FdF/src/file0.c
+++ FdF/src/file0.c
@@ -6,7 +6,7 @@
 /*   By: qbanet <qbanet@student.42perpignan.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 15:28:54 by qbanet            #+#    #+#             */
-/*   Updated: 2023/06/20 13:38:31 by qbanet           ###   ########.fr       */
+/*   Updated: 2023/06/20 14:23:15 by qbanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,23 +42,31 @@ static int	ft_count(char *s, t_map *map)
 static int	ft_pars_map(int fd, t_map *map)
 {
 	char		*line;
-	int			**mapp;
+	int			**mappi;
 	int			i;
 
 	line = NULL;
 	i = 0;
-	mapp = (int **)ft_calloc((map->nb_line), sizeof(int *));
-	if (!mapp)
+	mappi = (int **)ft_calloc((map->nb_line), sizeof(int *));
+	if (!mappi)
 		return (-1);
 	while (ft_gnl(fd, &line, 0))
 	{
-		mapp[i++] = ft_inted_line(line, map->nb_colon);
+		mappi[i++] = ft_inted_line(line, map->nb_colon);
 		free(line);
 	}
 	ft_gnl(fd, &line, 1);
-	map->map = mapp;
+	map->map = mappi;
 	return (0);
 }
+
+/*int	ft_color_map(char *s, t_map *map)
+{
+	int		fd;
+
+	fd = open(s, O_RDONLY);
+
+}*/
 
 int	create_map(t_map *map, char *s)
 {
@@ -73,6 +81,8 @@ int	create_map(t_map *map, char *s)
 		return (ft_error(ERROR_OPEN, NULL));
 	if (ft_pars_map(fd, map) == -1)
 		return (ft_error(ERROR_MAP, NULL));
+//	if (ft_color_map(s, map) == -1)
+//		return (ft_error(ERROR_MAP, NULL));
 	if (close(fd) == -1)
 		return (ft_error(ERROR_CLOSE, NULL));
 	ft_printf("line : %d, colon : %d\n\n", map->nb_line, map->nb_colon);
