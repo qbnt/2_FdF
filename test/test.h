e427cb15c66802f9fd4274ed73b80e6872860123
qbnt <quentin.banet@hotmail.fr>
2023-07-06 14:34:10 +0200

    oui

--- test/test.h
+++ test/test.h
@@ -6,7 +6,7 @@
 /*   By: parallels <parallels@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/28 18:32:38 by parallels         #+#    #+#             */
-/*   Updated: 2023/06/28 20:31:09 by parallels        ###   ########.fr       */
+/*   Updated: 2023/07/05 11:47:54 by parallels        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include<stdio.h>
 # include<stdlib.h>
+# include<math.h>
 # include"minilibx/mlx.h"
 
 # define WIDTH 800
@@ -47,6 +48,20 @@ typedef struct s_img
 	int		x;
 	int		y;
 }	t_img;
+
+typedef struct s_point
+{
+	int		x;
+	int		y;
+	int		x1;
+	int		y1;
+	int		z1;
+	int		x2;
+	int		y2;
+	int		z2;
+
+
+}
 
 typedef struct s_mlx
 {
