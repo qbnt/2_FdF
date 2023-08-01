25bf83ed0f7bfb9a8a1460eb8635a8eae39f1e91
qbnt <quentin.banet@hotmail.fr>
2023-08-01 16:05:25 +0200

    make save

--- Fdf_2/src/map_crea.c
+++ Fdf_2/src/map_crea.c
@@ -6,7 +6,7 @@
 /*   By: qbanet <qbanet@student.42perpignan.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/31 12:21:06 by qbanet            #+#    #+#             */
-/*   Updated: 2023/08/01 16:00:40 by qbanet           ###   ########.fr       */
+/*   Updated: 2023/08/01 16:05:15 by qbanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ void	mesure_map(t_map *map, int fd);
 void	save_map(t_map *map, int fd);
 void	set_value(t_map *map, char *line);
 void	set_color(t_map *map, char **elems);
+
 /***********************************************************/
 
 void	create_map(t_map *map, char *s)
